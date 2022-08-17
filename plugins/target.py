f163946be6515b4d2d77dd4792be5b94977344e4
Simon Willison <swillison@gmail.com>
2022-08-16 18:09:47 -0700

    Upgrade to new plugin hook, refs #3

--- plugins/target.py
+++ plugins/target.py
@@ -1,17 +1,17 @@
 from datasette import hookimpl
-from datasette.utils import TableFilter
+from datasette.filters import FilterArguments
 import json
 
 
 @hookimpl
-def table_filter():
-    async def inner(view, name, table, request):
-        # Hacky thing for ?_target=
-        extra_human_descriptions = []
+def filters_from_request(request, datasette, database):
+    db = datasette.get_database(database)
+    async def inner():
+        human_descriptions = []
         where_clauses = []
         params = {}
         # They can come in as JSON or as _target=foo&_target=bar
-        _targets_json = request.raw_args.get("_targets_json" or "")
+        _targets_json = request.args.get("_targets_json") or ""
         if _targets_json:
             try:
                 targets = json.loads(_targets_json)
@@ -19,16 +19,16 @@ def table_filter():
                 pass
         else:
             try:
-                targets = request.args["_target"]
+                targets = request.args.getlist("_target")
             except KeyError:
                 return None
+
         i = 0
         for target in targets:
-            target_name = (
-                await view.ds.execute(name, "select name from targets where id = :id", {
-                    "id": target
-                })
-            ).rows[0][0]
+            result = await db.execute("select name from targets where id = :id", {
+                "id": target
+            })
+            target_name = result.first()["name"]
             param = "target_{}".format(i)
             i += 1
             where_clauses.append("""
@@ -37,14 +37,14 @@ def table_filter():
                     where ad_targets.target_id = :{}
                 )
             """.format(param))
-            extra_human_descriptions.append(
+            human_descriptions.append(
                 "an ad_target was {}".format(target_name)
             )
             params[param] = target
 
-        return TableFilter(
-            human_description_extras=extra_human_descriptions,
+        return FilterArguments(
             where_clauses=where_clauses,
             params=params,
+            human_descriptions=human_descriptions,
         )
     return inner
