b2adab4a69b0ed80551a4bcba255d10c32d953ab
Simon Willison <swillison@gmail.com>
2022-08-16 18:15:55 -0700

    Deploy to Vercel instead of Cloud Run, refs #3

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-NAME="russian-ira-ads"
-PROJECT=$(gcloud config get-value project)
-IMAGE="gcr.io/$PROJECT/$NAME"
-
-gcloud builds submit --tag $IMAGE
-gcloud run deploy --allow-unauthenticated --platform=managed --image $IMAGE $NAME
+datasette publish vercel russian-ads.db \
+  --scope datasette \
+  --project russian-ads \
+  -m russian-ads-metadata.yaml \
+  --plugins-dir plugins \
+  --install datasette-publish-vercel \
+  --install datasette-json-html \
+  --install datasette-block-robots \
+  --install datasette-hashed-urls
